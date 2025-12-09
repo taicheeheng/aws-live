1a67a85693ca4457c8de14754833bc9b2d39246d
taicheeheng <cheehengtai2@gmail.com>
2025-12-09 15:27:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c52zax19d6ir.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "yong2125"
 customdb = "employee"
 custombucket = "taicheeheng-employee"
 customregion = "us-east-1"
